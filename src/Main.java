65e3a05ff1701223d5f7f678dd42712c2c6ba1f7
Grecha Kostya <grecakosta207@gmail.com>
2024-05-28 23:34:13 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,49 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+import java.util.Scanner;
+public class Main
+{
+    // Метод для подсчета кол-ва цифр в числе:
+    public static int countDigits(int number)
+    {
+        int count = 0;
+        while (number != 0)
+        {
+            number /= 10;
+            count++;
         }
+        return count;
+    }
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите последовательность чисел (через пробел): ");
+        String input = scanner.nextLine();
+        Scanner numberScanner = new Scanner(input);
+
+        // Инициализируем переменные:
+        int maxdigitCount = 0;
+        int minNumber = Integer.MAX_VALUE;
+
+        // Перебираем числа в последовательности, с помощью метода сканнера возвращая true, false:
+        while (numberScanner.hasNext())
+        {
+            // Считываем следующее число как строку:
+            String numberStr = numberScanner.next();
+
+            // Преобразуем строку в целое число с помощью метода Integer.parseInt:
+            int number = Integer.parseInt(numberStr);
+            // Считаем количество цифр в текущем числе:
+            int digitCount = countDigits(number);
+
+            // Проверка имеет ли текущее число больше цифр чем текущий max(если кол-во цифр одинаково, проверяем по значению)
+            if (digitCount > maxdigitCount | (digitCount == maxdigitCount && number < minNumber))
+            {
+                maxdigitCount = digitCount;
+                minNumber = number;
+            }
+        }
+
+        // Результат:
+        System.out.println("Минимальное число с максимальным количеством цифр: " + minNumber);
     }
 }
